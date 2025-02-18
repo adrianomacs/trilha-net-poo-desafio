c35f0cd31ceb19dee6663478cfb57af1b9c6139a
adrianomacs <adrianomacs@gmail.com>
2025-02-18 16:39:22 -0300

    feat: Classes Smartphone, Nokia e IPhone conforme diagrama

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
+Console.WriteLine("Smartphone Nokia:");
+Smartphone Nokia = new Nokia("123456789", "Nokia 3310", "123456789", 16);
+Nokia.Ligar();
+Nokia.ReceberLigacao();
+Nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\nSmartphone Iphone:");
+Smartphone Iphone = new Iphone("987654321", "Iphone 12", "987654321", 128);
+Iphone.Ligar();
+Iphone.ReceberLigacao();
+Iphone.InstalarAplicativo("Instagram");
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
